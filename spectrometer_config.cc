f704f0e1d236420722f614574a25e45c2378e21c
Anthony Steel <13as261@queensu.ca>
2018-03-12 14:58:27 -0400

    Remove large number of getter and setter methods but only validate uint32's

--- spectrometer_config.cc
+++ spectrometer_config.cc
@@ -1,217 +1,183 @@
-#include "avaspec.h"
-template<class TYPE> class SpectrometerConfig 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+
+typedef unsigned int uint32;
+
+typedef struct spectrometer_configuration_parameter 
+{
+    std::string name;
+    uint32 value;
+} spec_config_param;
+
+struct validator_map_entry 
+{
+    uint32* m_variable;
+    uint32 lower, upper;
+    bool (* validate)(uint32, uint32, uint32);
+}; 
+
+class SpectrometerConfigurationValidator
 {
     public:
-        SpectrometerConfig();
-        
-        bool setStartPixel(unsigned short val);
-        bool setStopPixel(unsigned short val);
-        bool setIntegrationTime(float val);
-        bool setIntegrationDelay(unsigned int val);
-        bool setNumberAverages(unsigned int val);
-        // Dynamic Dark Correction Type
-        bool setEnable(unsigned char val);
-        bool setForgetPercentage(unsigned char val);
-        // Smoothing Type
-        bool setSmoothPixel(unsigned short val);
-        bool setSmoothModel(unsigned char val);
-       // Trigger type
-        bool setMode(unsigned char val);
-        bool setSource(unsigned char val);
-        bool setSourceType(unsigned char val);
-        // Control Settings
-        bool setStrobeControl(unsigned short val);
-        bool setLaserDelay(unsigned int val);
-        bool setLaserWidth(unsigned int val);
-        bool setLaserWavelength(float val);
-        bool setStoreToRam(unsigned short val);
+        SpectrometerConfigurationValidator(std::string config_file_path);
 
-        unsigned short getStartPixel(unsigned short val);
-        unsigned short getStopPixel(unsigned short val);
-        float getIntegrationTime(float val);
-        unsigned int getIntegrationDelay(unsigned int val);
-        unsigned int getNumberAverages(unsigned int val);
-        // Dynamic Dark Correction Type
-        unsigned char getEnable(unsigned char val);
-        unsigned char getForgetPercentage(unsigned char val);
-        // Smoothing Type
-        unsigned short getSmoothPixel(unsigned short val);
-        unsigned char getSmoothModel(unsigned char val);
-       // Trigger type
-        unsigned char getMode(unsigned char val);
-        unsigned char getSource(unsigned char val);
-        unsigned char getSourceType(unsigned char val);
-        // Control Settings
-        unsigned short getStrobeControl(unsigned short val);
-        unsigned int getLaserDelay(unsigned int val);
-        unsigned int getLaserWidth(unsigned int val);
-        float getLaserWavelength(float val);
-        unsigned short getStoreToRam(unsigned short val);
+        std::vector<struct spectrometer_configuration_parameter>
+        validateUINT32(std::vector<struct spectrometer_configuration_parameter>
+                       config_vector);
+
 
     private:
-        unsigned short _start_pixel;
-        unsigned short _stop_pixel;
-        float _integration_time;
-        unsigned int _integration_delay;
-        unsigned int _number_averages;
-        unsigned char _enable;
-        unsigned char _forget_percentage;
-        unsigned short _smooth_pixel;
-        unsigned char _smooth_model;
-        unsigned char _mode;
-        unsigned char _source;
-        unsigned char _source_type;
-        unsigned short _strobe_control;
-        unsigned int _laser_delay;
-        unsigned int _laser_width;
-        float _laser_wavelength;
-        unsigned short _store_to_ram;
+        // Helper methods and attributes 
+        std::vector<std::string>
+        parseRow(std::string row);
 
-        bool setVal(TYPE* _property, 
-                    bool (*checkFunction) (TYPE, TYPE, TYPE),
-                    TYPE val, TYPE lower, TYPE upper);
+        uint32 
+        stringToNumber(std::string num_string);
 
-        bool checkInBounds(TYPE val, TYPE lower, TYPE upper);
+        std::unordered_map<std::string, struct validator_map_entry>
+        validator_lookup; 
+
+        std::unordered_map<std::string, bool(*)(uint32, uint32, uint32)>
+        check_function_lookup;
+
+        void defineCheckFunctionLookup();
+
 };
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setVal(TYPE* _property,
-                                bool (*checkFunction) (TYPE, TYPE, TYPE),
-                                TYPE val, TYPE lower, TYPE upper)
+bool checkInBounds(uint32 value, uint32 lower, uint32 upper)
 {
-    if(checkFunction(val, lower, upper))
+    return value >= lower && value <= upper;
+}
+
+void
+SpectrometerConfigurationValidator::defineCheckFunctionLookup()
+{
+    check_function_lookup["checkInBounds"] = &checkInBounds;
+}
+
+std::vector<std::string>
+SpectrometerConfigurationValidator::parseRow(std::string row)
+{
+
+    std::string column_value = std::string();
+    std::vector<std::string> parsed;
+    char COMMA = 0x2c, SPACE = 0x20;
+    for(char& c : row)
     {
-        *_property = val;
-        return 1;
+        if(c == COMMA) 
+        {
+            parsed.push_back(column_value);
+            column_value = std::string();
+        }
+        else if (c != SPACE)
+        {
+            column_value += c;
+        }
     }
-    else
+    parsed.push_back(column_value);
+
+    return parsed;
+}
+
+uint32 
+SpectrometerConfigurationValidator::stringToNumber(std::string num_string)
+{
+    uint32 uint32_type = 0U;
+    return std::stoi(num_string, nullptr, uint32_type);
+}
+
+SpectrometerConfigurationValidator::
+SpectrometerConfigurationValidator(std::string config_file_path)
+{
+    std::string row;
+    std::ifstream config_file;
+    std::vector<std::vector<std::string>> parsed_row;
+
+    defineCheckFunctionLookup();
+
+    config_file.open(config_file_path);
+    if(config_file.is_open())
     {
-        return 0;
+        while(std::getline(config_file, row))
+        {
+            parsed_row.push_back(parseRow(row));
+        }
+        for (const std::vector<std::string> &v : parsed_row)
+        {
+            std::string param_name = v[0], type = v[1]; 
+            uint32 lower = stringToNumber(v[2]), upper = stringToNumber(v[3]);
+
+            bool (*check_function) (uint32, uint32, uint32) = 
+            check_function_lookup[v[4]];
+
+            validator_lookup[param_name] =
+                validator_map_entry 
+                {
+                    .lower = lower,
+                    .upper = upper,
+                    .validate = check_function
+                };
+        }
+
+        config_file.close();
     }
 }
-                                  
+    
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::checkInBounds(TYPE val, TYPE lower, TYPE upper)
+std::vector<struct spectrometer_configuration_parameter>
+SpectrometerConfigurationValidator::
+validateUINT32(std::vector
+               <struct spectrometer_configuration_parameter> config_vector)
 {
-    return val >= lower && val <= upper;
+    for(auto param=config_vector.begin(); param!=config_vector.end(); ++param)
+    {
+        validator_map_entry entry = validator_lookup[param->name];
+
+        if(!entry.validate(param->value, entry.lower, entry.upper))
+        {
+            config_vector.erase(param);
+        }
+    }
+
+    return config_vector;
 }
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setStartPixel(unsigned short val)
-{
-    unsigned short lower = 0, upper = 4095;
-    return setVal(&_start_pixel, &checkInBounds, val, lower, upper);
-}
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setStopPixel(unsigned short val)
+int main()
 {
-    unsigned short lower = 0, upper = 4095;
-    return setVal(&_stop_pixel, &checkInBounds, val, lower, upper);
-}
+    std::vector<struct spectrometer_configuration_parameter>
+    configuration_vector = { 
+        spec_config_param {
+            .name = "start_pixel",
+            .value = 4100U
+        },
+        spec_config_param {
+            .name = "stop_pixel",
+            .value = 15U
+        }
+    };
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setIntegrationTime(float val)
-{
-    float lower = 0.002, upper = 600000.0;
-    return setVal(&_integration_time, &checkInBounds, val, lower, upper);
-}
+    SpectrometerConfigurationValidator validator("spectrometer_config.csv");
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setIntegrationDelay(unsigned int val)
-{
-    unsigned int lower = 0x0, upper = 0xFFFFFFFF;
-    return setVal(&_integration_delay, &checkInBounds, val, lower, upper);
-}
+    std::vector<struct spectrometer_configuration_parameter>
+    validated_configuration = validator.validateUINT32(configuration_vector);
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setNumberAverages(unsigned int val)
-{
-    unsigned int lower = 0x1, upper = 0xFFFFFFFF;
-    return setVal(&_number_averages, &checkInBounds, val, lower, upper);
-}
+    std::cout << std::endl;
+    std::cout << "validated configuration vector" << std::endl;
+    for(const auto& param : validated_configuration)
+    {
+        std::cout << "    " << param.name << std::endl;
+    }
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setEnable(unsigned char val)
-{
-    unsigned char lower = 0, upper = 1;
-    return setVal(&_enable, &checkInBounds, val, lower, upper);
-}
+    std::cout << std::endl;
+    std::cout << "unvalidated configuration vector" << std::endl;
+    for(const auto& param : configuration_vector)
+    {
+        std::cout << "    " << param.name << std::endl;
+    }
 
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setForgetPercentage(unsigned char val)
-{
-    unsigned char lower = 0, upper = 100;
-    return setVal( _forget_percentage, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setSmoothPixel(unsigned short val)
-{
-    unsigned short lower = 0, upper = 2048;
-    return setVal(&_smooth_pixel, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setSmoothModel(unsigned char val)
-{
-    unsigned char lower = 0, upper = 0;
-    return setVal(&_smooth_model, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setMode(unsigned char val)
-{
-    unsigned char lower = 0, upper = 2;
-    return setVal(&_mode, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setSource(unsigned char val)
-{
-    unsigned char lower = 0, upper = 1;
-    return setVal(&_source, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setSourceType(unsigned char val)
-{
-    unsigned char lower = 0, upper = 1;
-    return setVal(&_source_type, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setStrobeControl(unsigned short val)
-{
-    unsigned short lower = 0x0, upper = 0xFFFF;
-    return setVal(&_strobe_control, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setLaserDelay(unsigned int val)
-{
-    unsigned int lower = 0, upper = 0xFFFFFFFF;
-    return setVal(&_laser_delay, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setLaserWidth(unsigned int val)
-{
-    unsigned int lower = 0, upper = 0xFFFF;
-    return setVal(&_laser_width, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setLaserWavelength(float val)
-{
-    float lower = 0, upper = 0xFFFF;
-    return setVal(&_laser_wavelength, &checkInBounds, val, lower, upper);
-}
-
-template<class TYPE>
-bool SpectrometerConfig<TYPE>::setStoreToRam(unsigned short val)
-{
-    unsigned short lower = 0, upper = 0xFFFF;
-    return setVal(&_store_to_ram, &checkInBounds, val, lower, upper);
+    return 0;
 }
