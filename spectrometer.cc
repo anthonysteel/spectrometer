4514e836b4342f4b6d949e92e6fa5e43fa0523eb
anthonysteel <13as261@qeensu.ca>
2018-05-08 23:05:45 +0000

    Add delete statements for allcoation of data buffer and time label
    The run time memory corruption before was from the data buffer not being allocated enough space (2048 elements). The delete statements were removed by mistake in trying to debug the problem

--- spectrometer.cc
+++ spectrometer.cc
@@ -10,8 +10,6 @@ Spectrometer::Spectrometer(std::vector<spec_config_param> config_vector)
 
     for(const spec_config_param& param : config_vector)
     {
-        std::cout << param.name << " " << param.value << " " 
-        << SpecConfigValidator.validate(param) << std::endl;
         if (SpecConfigValidator.validate(param))
         {
             MeasConfigTypeBuilder meas_config_type_builder;
@@ -30,17 +28,17 @@ void Spectrometer::activate()
 	AvsIdentityType device_id_info[30];
 
 	int USB_port = 0; 
-    std::cout << std::endl;
-    std::cout << "AVS_Init(): " << AVS_Init(USB_port) << std::endl; 
 
-    std::cout << "Number of USB device: " << AVS_UpdateUSBDevices() << std::endl;
+    AVS_Init(USB_port); 
+
+    AVS_UpdateUSBDevices();
 	
 	unsigned int required_bytes;
 	AVS_GetList(sizeof(device_id_info), &required_bytes, device_id_info);
 
 	device_id = AVS_Activate( &device_id_info[0] );
 
-    std::cout << "AVS_PrepareMeasure(): " << AVS_PrepareMeasure(device_id, &spec_config) << std::endl;
+    AVS_PrepareMeasure(device_id, &spec_config);
 }
 
 std::vector<double> Spectrometer::measure()
@@ -49,13 +47,10 @@ std::vector<double> Spectrometer::measure()
 	
     unsigned int required_delay = 10; // ms
 
-    std::cout << "Device id: " << device_id << std::endl;
-
 	while( !AVS_PollScan(device_id) )
 	{
         usleep(required_delay);
 	}
-    std::cout << "hello" << std::endl;
 
     std::vector<double> spec_measurement_data;
 
@@ -65,9 +60,11 @@ std::vector<double> Spectrometer::measure()
 	for(int i = 0; i < 2048; i++)
 	{
 	   AVS_GetScopeData(device_id, time_label, data_buffer);
-       std::cout << "at " << i << " the value is " << data_buffer[i] << std::endl;
        spec_measurement_data.push_back(data_buffer[i]);
 	}
+
+    delete[] data_buffer;
+    delete[] time_label;
 
     return spec_measurement_data;
 }
