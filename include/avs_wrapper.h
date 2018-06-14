0375e579c143b8aaadf7afd70208051372ac50a1
Peter Wright <pewright77@gmail.com>
2018-06-14 15:23:16 -0400

    Fix compiler errors and more indentation

--- include/avs_wrapper.h
+++ include/avs_wrapper.h
@@ -5,24 +5,24 @@
 class AVSWrapper
 {
   private:
-      AvsIdentityType device_id_info[30];
-      AVSException SpecException;
+    AvsIdentityType device_id_info[30];
+    AVSException SpecException;
 
-      int Init(const int &usb_port);
+    int Init(const int &usb_port);
 
   public:
-      AVSWrapper(const int &usb_port);
+    AVSWrapper(const int &usb_port);
 
-      // int Init(const int &usb_port);
-      int Activate(const int &device_number);
-      void Deactivate(const int &device_id);
-      int UpdateUSBDevices(int *required_bytes);
+    // int Init(const int &usb_port);
+    int Activate(const int &device_number);
+    void Deactivate(const int &device_id);
+    int UpdateUSBDevices();
 
-      void PrepareMeasure(const int &device_id, MeasConfigType spec_config);
-      void Measure(const int &device_id, const short &num_measurements);
-      int PollScan(const int &device_id);
+    void PrepareMeasure(const int &device_id, MeasConfigType spec_config);
+    void Measure(const int &device_id, const short &num_measurements);
+    int PollScan(const int &device_id);
 
-      void GetScopeData(const int &device_id, const int *time_label, double *data_buffer);
-      void GetAnalogIn(const int &device_id, 
-          const int &analog_input_id, float *analog_reading);
-}
+    void GetScopeData(const unsigned int &device_id, unsigned int *time_label, double *data_buffer);
+    void GetAnalogIn(const int &device_id, 
+             const int &analog_input_id, float *analog_reading);
+};
--- src/avs_wrapper.cc
+++ src/avs_wrapper.cc
@@ -11,11 +11,11 @@ AVSWrapper::Init(const int &usb_port)
 {
   int init_return = AVS_Init(usb_port);
   if (init_return < 0) {
-      throw std::runtime_error("AVS_Init failed with: "
-                              + SpecException.lookup(init_return));
-    } else if (init_return == 0) {
-      throw std::runtime_error("AVS_Init failed with: NO_DEVICE_FOUND");
-    }
+    throw std::runtime_error("AVS_Init failed with: "
+                            + SpecException.lookup(init_return));
+  } else if (init_return == 0) {
+    throw std::runtime_error("AVS_Init failed with: NO_DEVICE_FOUND");
+  }
 
   return init_return;
 }
@@ -25,8 +25,8 @@ AVSWrapper::Activate(const int &device_number)
 {
   int activate_return = AVS_Activate(&device_id_info[device_number]);
   if (activate_return < 0) {
-      throw std::runtime_error("AVS_Activate failed with: "
-                              + SpecException.lookup(activate_return));
+    throw std::runtime_error("AVS_Activate failed with: "
+                            + SpecException.lookup(activate_return));
   }
 
   return activate_return;
@@ -37,13 +37,15 @@ AVSWrapper::Deactivate(const int &device_id)
 {
   bool deactivate_return = AVS_Deactivate(device_id);
   if (!deactivate_return) {
-      throw std::runtime_error("Spectrometer failed to deactivate, device ID incorrect");
+    throw std::runtime_error("Spectrometer failed to deactivate, device ID incorrect");
   }
 }
 
 int
-AVSWrapper::UpdateUSBDevices(int *required_bytes) {
+AVSWrapper::UpdateUSBDevices() {
   int update_usb_return = AVS_UpdateUSBDevices();
+  unsigned int required_bytes;
+
   if (update_usb_return < 0) {
       throw std::runtime_error("AVS_UpdateUSBDevices failed with: "
                               + SpecException.lookup(update_usb_return));
@@ -51,7 +53,6 @@ AVSWrapper::UpdateUSBDevices(int *required_bytes) {
       throw std::runtime_error("AVS_UpdateUSBDevices failed with: NO_DEVICE_FOUND");
   }
 
-  unsigned int required_bytes;
   int getlist_return = AVS_GetList(sizeof(device_id_info),
                                   &required_bytes, device_id_info);
   if (getlist_return < 0) {
@@ -66,10 +67,10 @@ AVSWrapper::UpdateUSBDevices(int *required_bytes) {
 
 void
 AVSWrapper::PrepareMeasure(const int &device_id, MeasConfigType spec_config) {
-  int prepare_measure_return = AVS_PrepareMeasure(device_id, spec_config);
+  int prepare_measure_return = AVS_PrepareMeasure(device_id, &spec_config);
   if (prepare_measure_return >= 0) {
-      throw std::runtime_error("AVS_PrepareMeasure failed with: "
-                              + SpecException.lookup(prepare_measure_return));
+    throw std::runtime_error("AVS_PrepareMeasure failed with: "
+                            + SpecException.lookup(prepare_measure_return));
   }
 }
 
@@ -77,26 +78,24 @@ void
 AVSWrapper::Measure(const int &device_id, const short &num_measurements) {
   int measure_callback_return = AVS_MeasureCallback(device_id, NULL, 1);
   if (measure_callback_return != 0) {
-      throw std::runtime_error("AVS_MeasureCallback failed with: "
-                              + SpecException.lookup(measure_callback_return));
+    throw std::runtime_error("AVS_MeasureCallback failed with: "
+                            + SpecException.lookup(measure_callback_return));
   }
-
-  return measure_callback_return;
 }
 
 int
 AVSWrapper::PollScan(const int &device_id) {
-  pollscan_return = AVS_PollScan(device_id);
+  int pollscan_return = AVS_PollScan(device_id);
   if (pollscan_return != 0) {
-      throw std::runtime_error("AVS_PollScan failed with: "
-                              + SpecException.lookup(pollscan_return);
+    throw std::runtime_error("AVS_PollScan failed with: "
+                            + SpecException.lookup(pollscan_return));
   }
 
   return pollscan_return;
 }
 
 void
-AVSWrapper::GetScopeData(const int &device_id, const int *time_label, double *data_buffer) {
+AVSWrapper::GetScopeData(const unsigned int &device_id, unsigned int *time_label, double *data_buffer) {
   int get_data_return = AVS_GetScopeData(device_id, time_label, data_buffer);
   if (get_data_return != 0) {
       throw std::runtime_error("AVS_GetScopeData threw: "
